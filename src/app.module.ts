e7696a136f580c14f82c807049200040c7ade857
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-09-06 20:39:33 +0300

    Add config file to support env variables

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DBUser } from './users/users.entity';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    AuthModule, 
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as unknown as 'postgres' | 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [DBUser],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot()
+  ],
+})
+export class AppModule {}
