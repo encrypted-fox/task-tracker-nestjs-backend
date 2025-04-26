b34d6472246ee32110bc58f66150eb816613709d
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:52:34 +0300

    Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  SwaggerModule.setup('api', app, () =>
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Tasks Tracker')
+        .setDescription('API documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build(),
+    ),
+  );
+
   await app.listen(8888);
 }
 
