f8688bf0840d306c9b142af560d54da023f71fce
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:56:41 +0300

    Remake columns to match new demands

--- src/modules/columns/columns.controller.ts
+++ src/modules/columns/columns.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { ColumnsEntity } from './columns.entity';
-import { ColumnsService } from './columns.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { ColumnsService } from './columns.service';
+import { ColumnsEntity } from './columns.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/columns')
 export class ColumnsController extends BaseController<
   ColumnsEntity,
@@ -21,5 +57,114 @@ export class ColumnsController extends BaseController<
     ];
 
     super(columnsFields, columnsService);
+  }
+
+  @ApiQueryDecorator(
+    'Get columns list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => ColumnsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<ColumnsEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get columns',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => ColumnsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<ColumnsEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get column',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => ColumnsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<ColumnsEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create column',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => ColumnsEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => ColumnsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'columns', action: 'CREATE' })
+  override async create(@Body() column: ColumnsEntity): Promise<ColumnsEntity> {
+    return super.create(column);
+  }
+
+  @ApiOperation({
+    summary: 'Update column',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => ColumnsEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => ColumnsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'columns', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: ColumnsEntity,
+  ): Promise<ColumnsEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete column',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'columns', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/columns/columns.entity.ts
+++ src/modules/columns/columns.entity.ts
@@ -5,36 +5,75 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { UsersEntity } from '../users/users.entity';
 import { BoardsEntity } from '../boards/boards.entity';
 import { ProjectsEntity } from '../projects/projects.entity';
 
 @Entity()
 export class ColumnsEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Title',
+    description: 'Title. String.',
+    required: true,
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Board id. Foreign Key boards id, number.',
+    type: () => BoardsEntity,
+  })
   @ManyToOne(() => BoardsEntity, { nullable: true })
   @JoinColumn({ name: 'board_id' })
   board?: BoardsEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Project id. Foreign Key projects id, number.',
+    type: () => ProjectsEntity,
+  })
   @ManyToOne(() => ProjectsEntity, { nullable: true })
   @JoinColumn({ name: 'project_id' })
   project?: ProjectsEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Creator id. Foreign Key users id, number.',
+    type: () => UsersEntity,
+  })
   @ManyToOne(() => UsersEntity)
   @JoinColumn({ name: 'creator_id' })
   creator: UsersEntity;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
