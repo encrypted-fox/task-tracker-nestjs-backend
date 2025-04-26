57ed8b0ebdb6ac85bbc8457bd4ac0a32eca4f3eb
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:55:58 +0300

    Remake priorities to match new demands

--- src/modules/priorities/priorities.controller.ts
+++ src/modules/priorities/priorities.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { PrioritiesEntity } from './priorities.entity';
-import { PrioritiesService } from './priorities.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { PrioritiesService } from './priorities.service';
+import { PrioritiesEntity } from './priorities.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/priorities')
 export class PrioritiesController extends BaseController<
   PrioritiesEntity,
@@ -19,5 +55,116 @@ export class PrioritiesController extends BaseController<
     ];
 
     super(prioritiesFields, prioritiesService);
+  }
+
+  @ApiQueryDecorator(
+    'Get priorities list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => PrioritiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<PrioritiesEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get priorities',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => PrioritiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<PrioritiesEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get priority',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => PrioritiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<PrioritiesEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create priority',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => PrioritiesEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => PrioritiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'priorities', action: 'CREATE' })
+  override async create(
+    @Body() priority: PrioritiesEntity,
+  ): Promise<PrioritiesEntity> {
+    return super.create(priority);
+  }
+
+  @ApiOperation({
+    summary: 'Update priority',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => PrioritiesEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => PrioritiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'priorities', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: PrioritiesEntity,
+  ): Promise<PrioritiesEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete priority',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'priorities', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/priorities/priorities.entity.ts
+++ src/modules/priorities/priorities.entity.ts
@@ -1,22 +1,48 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 @Entity()
 export class PrioritiesEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Title. Unique string.',
+  })
   @Column({ unique: true })
   title: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Order of a priority. Number.',
+  })
   @Column()
   value: number;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
