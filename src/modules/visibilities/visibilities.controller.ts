7c35243a9a04d8ec1d6616eb923d9e2351cb300f
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:53:40 +0300

    Remake visibilities to match new demands

--- src/modules/visibilities/visibilities.controller.ts
+++ src/modules/visibilities/visibilities.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { VisibilitiesEntity } from './visibilities.entity';
-import { VisibilitiesService } from './visibilities.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { VisibilitiesService } from './visibilities.service';
+import { VisibilitiesEntity } from './visibilities.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/visibilities')
 export class VisibilitiesController extends BaseController<
   VisibilitiesEntity,
@@ -20,5 +56,116 @@ export class VisibilitiesController extends BaseController<
     ];
 
     super(visibilitiesFields, visibilitiesService);
+  }
+
+  @ApiQueryDecorator(
+    'Get visibilities list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => VisibilitiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<VisibilitiesEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get visibilities',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => VisibilitiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<VisibilitiesEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get visibility',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => VisibilitiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<VisibilitiesEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create visibility',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => VisibilitiesEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => VisibilitiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'visibilities', action: 'CREATE' })
+  override async create(
+    @Body() visibility: VisibilitiesEntity,
+  ): Promise<VisibilitiesEntity> {
+    return super.create(visibility);
+  }
+
+  @ApiOperation({
+    summary: 'Update visibility',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => VisibilitiesEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => VisibilitiesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'visibilities', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: VisibilitiesEntity,
+  ): Promise<VisibilitiesEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete visibility',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'visibilities', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/visibilities/visibilities.entity.ts
+++ src/modules/visibilities/visibilities.entity.ts
@@ -5,29 +5,64 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { VisibilityTypesEntity } from '../visibilityTypes/visibilityTypes.entity';
 
 @Entity()
 export class VisibilitiesEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Title',
+    description: 'Title. String.',
+    required: true,
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    example: {
+      id: 1,
+    },
+    description: 'Visibility object. JSON.',
+  })
   @Column({ type: 'json', nullable: true })
   object?: Record<string, unknown>;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Visibility type id. Foreign Key visibilityTypes id, number.',
+    type: () => VisibilityTypesEntity,
+  })
   @ManyToOne(() => VisibilityTypesEntity)
   @JoinColumn({ name: 'type_id' })
   visibilityType: VisibilityTypesEntity;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
