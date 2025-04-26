1058df684747ba017f6cfe716feb0196e14fd10b
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:56:22 +0300

    Remake invites to match new demands

--- src/modules/invites/invites.controller.ts
+++ src/modules/invites/invites.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { InvitesEntity } from './invites.entity';
-import { InvitesService } from './invites.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { InvitesService } from './invites.service';
+import { InvitesEntity } from './invites.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/invites')
 export class InvitesController extends BaseController<
   InvitesEntity,
@@ -20,5 +56,114 @@ export class InvitesController extends BaseController<
     ];
 
     super(invitesFields, invitesService);
+  }
+
+  @ApiQueryDecorator(
+    'Get invites list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => InvitesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<InvitesEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get invites',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => InvitesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<InvitesEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get invite',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => InvitesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<InvitesEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create invite',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => InvitesEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => InvitesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'invites', action: 'CREATE' })
+  override async create(@Body() invite: InvitesEntity): Promise<InvitesEntity> {
+    return super.create(invite);
+  }
+
+  @ApiOperation({
+    summary: 'Update invite',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => InvitesEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => InvitesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'invites', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: InvitesEntity,
+  ): Promise<InvitesEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete invite',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'invites', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/invites/invites.entity.ts
+++ src/modules/invites/invites.entity.ts
@@ -5,31 +5,64 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { UsersEntity } from '../users/users.entity';
 import { TeamsEntity } from '../teams/teams.entity';
 
 @Entity()
 export class InvitesEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @ApiProperty({
+    example: 1,
+    description: 'Invite key. UUID string.',
+  })
+  @Column({ unique: true })
   value: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Creator id. Foreign Key users id, number.',
+    type: () => UsersEntity,
+  })
   @ManyToOne(() => UsersEntity)
   @JoinColumn({ name: 'creator_id' })
   creator: UsersEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Team id. Foreign Key teams id, number.',
+    type: () => TeamsEntity,
+  })
   @ManyToOne(() => TeamsEntity)
   @JoinColumn({ name: 'team_id' })
   team: TeamsEntity;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
