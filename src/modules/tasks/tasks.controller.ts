d058534c249a68484abb174d7829b8c773191fad
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:55:11 +0300

    Remake tasks to match new demands

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { TasksEntity } from './tasks.entity';
-import { TasksService } from './tasks.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { TasksService } from './tasks.service';
+import { TasksEntity } from './tasks.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/tasks')
 export class TasksController extends BaseController<TasksEntity, TasksService> {
   constructor(private tasksService: TasksService) {
@@ -25,5 +61,114 @@ export class TasksController extends BaseController<TasksEntity, TasksService> {
     ];
 
     super(tasksFields, tasksService);
+  }
+
+  @ApiQueryDecorator(
+    'Get tasks list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => TasksEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<TasksEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get tasks',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => TasksEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<TasksEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get task',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => TasksEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<TasksEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create task',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => TasksEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => TasksEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'tasks', action: 'CREATE' })
+  override async create(@Body() task: TasksEntity): Promise<TasksEntity> {
+    return super.create(task);
+  }
+
+  @ApiOperation({
+    summary: 'Update task',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => TasksEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => TasksEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'tasks', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: TasksEntity,
+  ): Promise<TasksEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete task',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'tasks', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/tasks/tasks.entity.ts
+++ src/modules/tasks/tasks.entity.ts
@@ -7,6 +7,9 @@ import {
   ManyToMany,
   JoinTable,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { UsersEntity } from '../users/users.entity';
 import { ProjectsEntity } from '../projects/projects.entity';
 import { BoardsEntity } from '../boards/boards.entity';
@@ -17,25 +20,58 @@ import { VisibilitiesEntity } from '../visibilities/visibilities.entity';
 
 @Entity()
 export class TasksEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Title',
+    description: 'Title. String.',
+    required: true,
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    example: 'Description',
+    description: 'Description. String.',
+  })
   @Column({ nullable: true })
   description?: string;
 
+  @ApiProperty({
+    example: 24,
+    description: 'Estimated time. Number of minutes.',
+  })
   @Column({ nullable: true })
-  estimate?: string;
+  estimate?: number;
 
+  @ApiProperty({
+    example: [
+      'https://ru.wikipedia.org/static/images/project-logos/ruwiki.png',
+    ],
+    description: 'Attachment links array. String.',
+  })
   @Column('text', { nullable: true, array: true })
   attachments?: string[];
 
+  @ApiProperty({
+    example: 1,
+    description: 'Priority id. Foreign Key priorities id, number.',
+    type: () => PrioritiesEntity,
+  })
   @ManyToOne(() => PrioritiesEntity, { nullable: true })
   @JoinColumn({ name: 'priority_id' })
   priority?: PrioritiesEntity;
 
+  @ApiProperty({
+    example: [1],
+    description: 'Tag ids array. Foreign Key tags id, number.',
+    type: () => TagsEntity,
+  })
   @ManyToMany(() => TagsEntity, { nullable: true })
   @JoinTable({
     name: 'task_tags',
@@ -44,26 +80,56 @@ export class TasksEntity {
   })
   tags?: TagsEntity[];
 
+  @ApiProperty({
+    example: 1,
+    description: 'Column id. Foreign Key columns id, number.',
+    type: () => ColumnsEntity,
+  })
   @ManyToOne(() => ColumnsEntity, { nullable: true })
   @JoinColumn({ name: 'column_id' })
   column?: ColumnsEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Board id. Foreign Key boards id, number.',
+    type: () => BoardsEntity,
+  })
   @ManyToOne(() => BoardsEntity, { nullable: true })
   @JoinColumn({ name: 'board_id' })
   board?: BoardsEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Project id. Foreign Key projects id, number.',
+    type: () => ProjectsEntity,
+  })
   @ManyToOne(() => ProjectsEntity, { nullable: true })
   @JoinColumn({ name: 'project_id' })
   project?: ProjectsEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Creator id. Foreign Key users id, number.',
+    type: () => UsersEntity,
+  })
   @ManyToOne(() => UsersEntity)
   @JoinColumn({ name: 'creator_id' })
   creator: UsersEntity;
 
-  @ManyToOne(() => VisibilitiesEntity)
+  @ApiProperty({
+    example: 1,
+    description: 'Visibility id. Foreign Key visibilities id, number.',
+    type: () => VisibilitiesEntity,
+  })
+  @ManyToOne(() => VisibilitiesEntity, { nullable: true })
   @JoinColumn({ name: 'visibility_id' })
-  visibility: VisibilitiesEntity;
+  visibility?: VisibilitiesEntity;
 
+  @ApiProperty({
+    example: [1],
+    description: 'User ids array. Foreign Key users id, number.',
+    type: () => UsersEntity,
+  })
   @ManyToMany(() => UsersEntity)
   @JoinTable({
     name: 'task_users',
@@ -72,12 +138,24 @@ export class TasksEntity {
   })
   relatedUsers: UsersEntity[];
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
