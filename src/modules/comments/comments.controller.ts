a75930b29c221fd6095d69f37ceeecb9f1b46077
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:56:35 +0300

    Remake comments to match new demands

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,8 +1,41 @@
-import { Controller } from '@nestjs/common';
-import { CommentsEntity } from './comments.entity';
-import { CommentsService } from './comments.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth, ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse, ApiOperation, ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { CommentsService } from './comments.service';
+import { CommentsEntity } from './comments.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/comments')
 export class CommentsController extends BaseController<
   CommentsEntity,
@@ -22,5 +55,116 @@ export class CommentsController extends BaseController<
     ];
 
     super(commentsFields, commentsService);
+  }
+
+  @ApiQueryDecorator(
+    'Get comments list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => CommentsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<CommentsEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get comments',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => CommentsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<CommentsEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get comment',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => CommentsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<CommentsEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create comment',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => CommentsEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => CommentsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'comments', action: 'CREATE' })
+  override async create(
+    @Body() comment: CommentsEntity,
+  ): Promise<CommentsEntity> {
+    return super.create(comment);
+  }
+
+  @ApiOperation({
+    summary: 'Update comment',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => CommentsEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => CommentsEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'comments', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: CommentsEntity,
+  ): Promise<CommentsEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete comment',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'comments', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/comments/comments.entity.ts
+++ src/modules/comments/comments.entity.ts
@@ -5,39 +5,83 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { UsersEntity } from '../users/users.entity';
 import { CommentTypesEntity } from '../commentTypes/commentTypes.entity';
 import { TasksEntity } from '../tasks/tasks.entity';
 
 @Entity()
 export class CommentsEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Description',
+    description: 'Description. String.',
+  })
   @Column({ nullable: true })
   description?: string;
 
+  @ApiProperty({
+    example: [
+      'https://ru.wikipedia.org/static/images/project-logos/ruwiki.png',
+    ],
+    description: 'Attachment links array. String.',
+  })
   @Column('text', { nullable: true, array: true })
   attachments?: string[];
 
+  @ApiProperty({
+    example: 1,
+    description: 'Comment type id. Foreign Key commentTypes id, number.',
+    type: () => CommentTypesEntity,
+  })
   @ManyToOne(() => CommentTypesEntity)
   @JoinColumn({ name: 'type_id' })
   commentType: CommentTypesEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Task id. Foreign Key tasks id, number.',
+    type: () => TasksEntity,
+  })
   @ManyToOne(() => TasksEntity)
   @JoinColumn({ name: 'task_id' })
   task: TasksEntity;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Creator id. Foreign Key users id, number.',
+    type: () => UsersEntity,
+  })
   @ManyToOne(() => UsersEntity)
-  @JoinColumn({ name: 'user_id' })
+  @JoinColumn({ name: 'creator_id' })
   creator: UsersEntity;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
