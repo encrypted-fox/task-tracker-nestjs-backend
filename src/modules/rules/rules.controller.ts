ea8fdfad554fe678a5a7488f6ef8f5bcf2379a0b
Сучков Ярослав <ys.suchkov@sov-teh.com>
2025-04-26 19:55:24 +0300

    Remake rules to match new demands

--- src/modules/rules/rules.controller.ts
+++ src/modules/rules/rules.controller.ts
@@ -1,8 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { RulesEntity } from './rules.entity';
-import { RulesService } from './rules.service';
-import { BaseController } from '../../base/BaseController';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiQueryDecorator } from '../../helpers/ApiQueryDecorator';
+
+import { I18n, I18nContext } from 'nestjs-i18n';
+
+import {
+  BaseController,
+  type BaseQueryParams,
+  type Response,
+} from '../../base/BaseController';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { LogAction } from '../logs/logs.decorator';
+
+import { RulesService } from './rules.service';
+import { RulesEntity } from './rules.entity';
+
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('api/rules')
 export class RulesController extends BaseController<RulesEntity, RulesService> {
   constructor(private rulesService: RulesService) {
@@ -17,5 +53,114 @@ export class RulesController extends BaseController<RulesEntity, RulesService> {
     ];
 
     super(rulesFields, rulesService);
+  }
+
+  @ApiQueryDecorator(
+    'Get rules list',
+    'Returns structured data for table rendering with headers and metadata',
+  )
+  @ApiOkResponse({ type: () => RulesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('/list')
+  override async getList(
+    @I18n() i18n: I18nContext,
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<RulesEntity>> {
+    return super.getList(i18n, queryParams);
+  }
+
+  @ApiQueryDecorator(
+    'Get rules',
+    'Returns structured data with metadata and entities',
+  )
+  @ApiOkResponse({ type: () => RulesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get('')
+  override async getAll(
+    @Query() queryParams: BaseQueryParams,
+  ): Promise<Response<RulesEntity>> {
+    return super.getAll(queryParams);
+  }
+
+  @ApiOperation({
+    summary: 'Get rule',
+    description: 'Returns entity by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse({ type: () => RulesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  override async get(@Param('id') id: number): Promise<RulesEntity> {
+    return super.get(id);
+  }
+
+  @ApiOperation({
+    summary: 'Create rule',
+    description: 'Creates entity',
+  })
+  @ApiBody({
+    type: () => RulesEntity,
+    description: 'Entity to create',
+  })
+  @ApiCreatedResponse({ type: () => RulesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('')
+  @LogAction({ entity: 'rules', action: 'CREATE' })
+  override async create(@Body() rule: RulesEntity): Promise<RulesEntity> {
+    return super.create(rule);
+  }
+
+  @ApiOperation({
+    summary: 'Update rule',
+    description: 'Updates entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiBody({
+    type: () => RulesEntity,
+    description: 'Entity to create',
+  })
+  @ApiOkResponse({ type: () => RulesEntity })
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  @LogAction({ entity: 'rules', action: 'UPDATE' })
+  override async update(
+    @Param('id') id: number,
+    @Body() entity: RulesEntity,
+  ): Promise<RulesEntity> {
+    return super.update(id, entity);
+  }
+
+  @ApiOperation({
+    summary: 'Delete rule',
+    description: 'Delete entity',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 20,
+    description: 'Id of an entity',
+  })
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  @LogAction({ entity: 'rules', action: 'DELETE' })
+  override async delete(@Param('id') id: number): Promise<void> {
+    return super.delete(id);
   }
 }
--- src/modules/rules/rules.entity.ts
+++ src/modules/rules/rules.entity.ts
@@ -5,29 +5,62 @@ import {
   ManyToOne,
   JoinColumn,
 } from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 import { RolesEntity } from '../roles/roles.entity';
 
 @Entity()
 export class RulesEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'Id. Primary Key, number.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Title',
+    description: 'Title. String.',
+    required: true,
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'Value of rule. Boolean.',
+  })
   @Column()
   value: boolean;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Role id. Foreign Key role id, number.',
+    type: () => RolesEntity,
+  })
   @ManyToOne(() => RolesEntity)
   @JoinColumn({ name: 'role_id' })
   role: RolesEntity;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Created date. ISO string. UTC 0',
+  })
   @Column()
   createdAt: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Updated date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   updatedAt?: string;
 
+  @ApiProperty({
+    example: '2011-10-05T14:48:00.000Z',
+    description: 'Deleted date. ISO string. UTC 0',
+  })
   @Column({ nullable: true })
   deletedAt?: string;
 }
